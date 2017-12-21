556907b16cd022e3141af25a35cabe55efef4f38
Alex <alex_lazarev@mail.ua>
2017-12-21 19:53:21 +0200

    JPA config

--- src/main/java/alex/stud/config/SpringConfig.java
+++ src/main/java/alex/stud/config/SpringConfig.java
@@ -1,16 +1,75 @@
 package alex.stud.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = {"alex.stud.dao","alex.stud.service","alex.stud.entity"})
+@PropertySource(value = "classpath:application.properties")
 public class SpringConfig {
+
+    @Autowired
+    private Environment environment;
+
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(environment.getProperty("datasource.url"));
+        dataSource.setUsername(environment.getProperty("datasource.username"));
+        dataSource.setPassword(environment.getProperty("datasource.password"));
+        dataSource.setDriverClassName(environment.getProperty("datasource.driverClassName"));
+        return dataSource;
+    }
+
+    @Bean
+    public Properties getParameters() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        return properties;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(getDataSource());
+        entityManagerFactory.setJpaProperties(getParameters());
+        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactory.setPackagesToScan("com.test.bu.entity");
+        return entityManagerFactory;
+    }
+
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+
+
+
+
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
 
     /*@Bean
     public CustomerService getCustomerService(){
@@ -21,19 +80,4 @@ public class SpringConfig {
     public CustomerDao getCustomerDao(){
         return new CustomerDaoImpl();
     }*/
-
-    @Bean
-    public JdbcTemplate getJdbcTemplate(){
-        return new JdbcTemplate(getDataSource());
-    }
-
-    @Bean
-    public DataSource getDataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/Shop");
-        dataSource.setUsername("root");
-        dataSource.setPassword("QAZxsw1!");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        return dataSource;
-    }
 }
